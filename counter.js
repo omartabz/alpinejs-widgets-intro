74d194341a6ed95b7f9c5c69789d7e74d3ecd794
Omar Tabengwa <166306416+omartabz@users.noreply.github.com>
2024-07-05 20:49:03 +0200

    Update counter.js

--- counter.js
+++ counter.js
@@ -1,17 +1,17 @@
-function Counter(maxValue = 50) {
+function Counter(maxValue = 10) {
   return {
       count: 0,
       maxValue: maxValue,
       increment() {
-        //   if (this.count < this.maxValue) {
-              this.count+=1;
+      if (this.count < this.maxValue) {
+              this.count++;
               console.log("increment function")
-        //   }
+          }
       },
       decrement() {
-        //   if (this.count > 0) {
+         if (this.count > 0) {
               this.count--;
-        //   }
+          }
       },
       reset() {
           this.count = 0;
